ad378c2721292e79de20ff1dc4f18d5491a230b8
Anthony Maudry <anthony.maudry@thuata.com>
2016-05-14 14:35:20 +0200

    added Listener

---
+++ Resources/public/js/lib/Listener.js
@@ -0,0 +1,22 @@
+var listener = new function ActionListener() {
+    var registered = [];
+
+    this.register = function (action, callback) {
+        registered[action] = callback;
+    };
+
+    this.unregister = function (action) {
+        registered[action] = null;
+        delete(registered[action]);
+    };
+
+    document.querySelector('body').addEventListener('click', function (e) {
+        var target = e.target;
+        do {
+            var action = target.dataset.action;
+            if (action && registered[action]) {
+                registered[action].apply(target, [e]);
+            }
+        } while (target = target.parentNode);
+    }, true);
+};
---
+++ ThuataJavascriptBundle.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Thuata\JavasriptBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class ThuataJavasriptBundle
+ *
+ * @package Thuata\JavasriptBundle
+ *
+ * @author Anthony Maudry <anthony.maudry@thuata.com>
+ */
+class ThuataJavasriptBundle extends Bundle
+{
+}
